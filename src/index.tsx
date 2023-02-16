459435e52eb31b4a32ace8fbea8aa7165e0bb949
kirillkazakoff <kekazavr228@gmail.com>
2023-02-16 14:29:20 +1100

    linter was set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,40 @@
-import App from "./taskpane/components/App";
-import { AppContainer } from "react-hot-loader";
-import { initializeIcons } from "@fluentui/font-icons-mdl2";
-import { ThemeProvider } from "@fluentui/react";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable global-require */
+import { AppContainer } from 'react-hot-loader';
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+import { ThemeProvider } from '@fluentui/react';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './taskpane/components/App';
 
-/* global document, Office, module, require */
+/* global Office */
 
 initializeIcons();
 
 let isOfficeInitialized = false;
 
-const title = "Contoso Task Pane Add-in";
+const title = 'Contoso Task Pane Add-in';
 
 const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <ThemeProvider>
-        <Component title={title} isOfficeInitialized={isOfficeInitialized} />
-      </ThemeProvider>
-    </AppContainer>,
-    document.getElementById("container")
-  );
+    ReactDOM.render(
+        <AppContainer>
+            <ThemeProvider>
+                <Component title={title} isOfficeInitialized={isOfficeInitialized} />
+            </ThemeProvider>
+        </AppContainer>,
+        document.getElementById('container'),
+    );
 };
 
 /* Render application after Office initializes */
 Office.onReady(() => {
-  isOfficeInitialized = true;
-  render(App);
+    isOfficeInitialized = true;
+    render(App);
 });
 
 if ((module as any).hot) {
-  (module as any).hot.accept("./taskpane/components/App.tsx", () => {
-    const NextApp = require("./taskpane/components/App.tsx").default;
-    render(NextApp);
-  });
+    (module as any).hot.accept('./taskpane/components/App.tsx', () => {
+        const NextApp = require('./taskpane/components/App.tsx').default;
+        render(NextApp);
+    });
 }
