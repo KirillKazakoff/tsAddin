9f817482d0ccf6001b8ac4eee48f119a3f734b1e
kirillkazakoff <kekazavr228@gmail.com>
2023-09-05 09:44:55 +1100

    migration to react v18 + hot loader replaced on react-refresh

--- src/app/components/SectionErrorHOC.tsx
+++ src/app/components/SectionErrorHOC.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
-import { renderToString } from 'react-dom/server';
 import { TableStatusT } from '../types/typesTables';
-import {createRoot} from 'react-dom/client';
 
-
-declare module "react-dom/client" {
-    // typing module default export as `any` will allow you to access its members without compiler warning
-    var createRoot: any;
-    export {createRoot};
-  }
+// declare module "react-dom/client" {
+//     var createRoot: any;
+//     export {createRoot};
+//   }
 
 type Props = { status: TableStatusT; children: React.ReactElement; title: string };
 
-const isHtml = (c: React.ReactElement) => {
-    const div = document.createElement('div');
-    const root = 
-};
+// const isHtml = (c: React.ReactElement) => {
+//     const div = document.createElement('div');
+//     const root =
+// };
 
 export const SectionErrorHOC = observer(({ status, children, title }: Props) => {
     if (status.statusType !== 'ok') {
         return (
             <div className='error-route'>
+                <div>hdssd</div>
                 <h2>{title}</h2>
                 <h3 className='error-title mb0'>{status.title}</h3>
                 <div className='error-desc'>{status.desc}</div>
@@ -30,7 +27,7 @@ export const SectionErrorHOC = observer(({ status, children, title }: Props) => 
     }
 
     // if empty component children
-    if (!isHtml(children)) return null;
+    // if (!isHtml(children)) return null;
 
     return (
         <>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const render = () => {
-    ReactDOM.render(<App />, document.getElementById('container'));
+    const container = document.getElementById('container');
+    const root = createRoot(container);
+    root.render(<App />);
 };
+
+// const render = () => {
+//     ReactDOM.render(<App />, document.getElementById('container'));
+// };
 
 Office.onReady(() => {
     render();
